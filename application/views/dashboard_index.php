f57a8d4a6e31d7e5d70cd2e6254b3bfe3139415d
bryan182 <bryanvela182@gmail.com>
2015-09-29 17:34:33 -0500

    Proyecto dashboard de aplicación taxi

---
+++ application/views/dashboard_index.php
@@ -0,0 +1,65 @@
+ 
+     
+<div id="principal">
+    <div id="cuadro">
+        <div class="row"> 
+            <div class="col l4 m12 s12">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        imagen
+                    </div>
+                    <div class="card-content amber darken-2">
+                        <p>contenido</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        imagen
+                    </div>
+                    <div class="card-content amber darken-2">
+                        <p>contenido</p>
+                    </div>
+                </div>
+            </div>
+
+            <div id="mapa" class="col l8 m12 s12">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        imagen
+                    </div>
+                    <div class="card-content amber darken-2">
+                        <p>contenido</p>
+                    </div>
+                </div>                
+            </div>
+
+           
+
+        </div>
+        <div class="row">
+            <div class="col l6 m6 s12">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        imagen
+                    </div>
+                    <div class="card-content amber darken-2">
+                        <p>contenido</p>
+                    </div>
+                </div>
+            </div>
+
+             <div class="col l6 m6 s12">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        imagen
+                    </div>
+                    <div class="card-content amber darken-2">
+                        <p>contenido</p>
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+    </div>
+</div>
